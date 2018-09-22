a4e36487a9730bd1120aa42403170b763dd02a91
sa1987 <aswathi.tcs@gmail.com>
2018-09-22 19:35:28 +0000

    git configure

---
+++ gitconfigure.sh
@@ -0,0 +1,10 @@
+git init
+git config --global user.email aswathi.tcs@gmail.com
+git config --global user.name sa1987
+git add .
+git commit -m "first commit"
+git remote add origin https://github.com/sa1987/ansible.git
+git push -u origin master
+git pull
+git push -u origin master
+
